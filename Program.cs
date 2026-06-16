e43d102aee2c57dbaf724ae2b51a7e3b8b405679
TJJohn412 <tjjohn059@gmail.com>
2026-06-16 10:48:49 +0530

    finall

--- Program.cs
+++ Program.cs
@@ -12,6 +12,19 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("UsersDb"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:4200",
+                "http://localhost:59059"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,10 +34,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/test", () => "Backend Updated");
 
 app.Run();
